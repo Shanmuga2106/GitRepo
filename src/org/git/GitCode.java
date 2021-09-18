995082127e79fbc13aebb4ae30f33bfe494d5bf7
Shanmuga2106 <vallishanmuga6@gmail.com>
2021-09-18 22:44:50 +0530

    Code Created

---
+++ src/org/git/GitCode.java
@@ -0,0 +1,21 @@
+package org.git;
+
+public class GitCode {
+	private void empid() {
+		System.out.println("67034");
+	}
+	private void empname() {
+		System.out.println("sangi");
+	}
+	private void empcity() {
+		System.out.println("Chennai");
+	}
+	
+	public static void main(String[] args) {
+		GitCode gc = new GitCode();
+		gc.empid();
+		gc.empname();
+		gc.empcity();
+	}
+
+}
